54f17915f0959336bd51f20420ac550afd826c8c
Christopher Probst <christopher.probst@uni-duesseldorf.de>
2014-12-29 15:38:43 +0100

    Improve benchmark

--- app.go
+++ app.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"log"
 	"os"
 	"time"
@@ -11,43 +12,63 @@ func main() {
 		log.Fatal("No path specified")
 	}
 
+	// Create new benchmark file
 	f, err := os.Create(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer os.Remove(os.Args[1])
 
-	seconds := 3
-	duration := time.Duration(seconds) * time.Second
-	written := make(chan int)
+	// Create garbage
+	buf := make([]byte, 32*1024)
+	for i := range buf {
+		buf[i] = byte(i % 255)
+	}
 
-	go func() {
-		buf := make([]byte, 32*1024)
-
-		for i := range buf {
-			buf[i] = byte(i % 255)
+	// Write file
+	t := time.Now()
+	total := 1024 * 1024 * 1024
+	for i := 0; i < total; {
+		l := len(buf)
+		if total-i < l {
+			l = total - i
 		}
 
-		total := 0
-		defer func() {
-			written <- total
-		}()
-
-		for {
-			n, err := f.Write(buf)
-			total += n
-			if err != nil {
-				return
-			}
+		n, err := f.Write(buf[:l])
+		i += n
+		if err != nil {
+			return
 		}
-	}()
-
-	time.Sleep(duration)
+	}
 	f.Close()
-	total := <-written
-
-	amount := float64(total) / 1024 / 1024
-	rate := amount / float64(seconds)
-	log.Println("Written:", amount, "mb")
+	d := time.Now().Sub(t)
+	mb := total / 1024 / 1024
+	rate := float64(mb) / d.Seconds()
+	log.Println("Written:", mb, "mb")
 	log.Println("Write rate:", rate, "mb/s")
+
+	// Open benchmark file
+	f, err = os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Read file
+	t = time.Now()
+	i := 0
+	for {
+		n, err := f.Read(buf)
+		i += n
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatalln(err)
+		}
+	}
+	f.Close()
+	d = time.Now().Sub(t)
+	mb = i / 1024 / 1024
+	rate = float64(mb) / d.Seconds()
+	log.Println("Read:", mb, "mb")
+	log.Println("Read rate:", rate, "mb/s")
 }
